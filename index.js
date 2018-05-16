f7733af8f60100c6a19251fca829ed8705a09153
mashur.hossain@gmail.com <mashur.hossain@gmail.com>
2018-05-16 03:59:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -40,3 +40,26 @@ describe('sayHiToGrandma(string)', function() {
     expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
   })
 })
+
+switch (new Date().getDay()) {
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+}
