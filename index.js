ffb1932f98c76c24a823f93fdc877e577cf3da70
mashur.hossain@gmail.com <mashur.hossain@gmail.com>
2018-05-16 04:06:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
     
    } else if {
-     
-    
+   if (string.toUpperCase())
+   return "YES INDEED!"
 
 } else {
   return "YES INDEED!"
